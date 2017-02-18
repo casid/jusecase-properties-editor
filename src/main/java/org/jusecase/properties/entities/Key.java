327adae57a9d06fed3685a7fe9a72854537d33a4
Andreas Hager <andy@mazebert.com>
2017-02-18 20:53:34 +0100

    Forgot to commit the entity

---
+++ src/main/java/org/jusecase/properties/entities/Key.java
@@ -0,0 +1,48 @@
+package org.jusecase.properties.entities;
+
+public class Key implements Comparable<Key> {
+    private final String key;
+
+    public void setState(KeyState state) {
+        this.state = state;
+    }
+
+    private KeyState state;
+
+    public Key(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public KeyState getState() {
+        return state;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Key key1 = (Key) o;
+
+        return key.equals(key1.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return key.hashCode();
+    }
+
+    @Override
+    public int compareTo(Key o) {
+        return key.compareTo(o.key);
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
+}
