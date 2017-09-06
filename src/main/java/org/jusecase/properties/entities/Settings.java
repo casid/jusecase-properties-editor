12e1e44c3868d8f7a4b3dc5650558885d83b6b87
Andreas Hager <andreas@chrono24.com>
2017-09-06 15:59:25 +0200

    Allow plugin specific settings

--- src/main/java/org/jusecase/properties/entities/Settings.java
+++ src/main/java/org/jusecase/properties/entities/Settings.java
@@ -1,5 +1,10 @@
 package org.jusecase.properties.entities;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Settings {
     public String lastFile;
+    public Map<String, Object> pluginSettings = new HashMap<>();
 }
