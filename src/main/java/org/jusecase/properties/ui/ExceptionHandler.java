9a530bab83074b13738915c561a3fd7c960d4566
Andreas Hager <andreas@chrono24.com>
2018-07-24 17:33:15 +0200

    Better error handling, in case swing is broken.

--- src/main/java/org/jusecase/properties/ui/ExceptionHandler.java
+++ src/main/java/org/jusecase/properties/ui/ExceptionHandler.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-        JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         e.printStackTrace(); // Since we have no logger yet ;-)
+        JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
     }
 }
